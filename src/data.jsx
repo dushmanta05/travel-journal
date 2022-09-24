1efab3690d015a28edb1174138321605418e7e42
Diilu Behera <imdilub@gmail.com>
2022-09-24 10:55:38 +0530

    Data file added to render through react props

---
+++ src/data.jsx
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Amalfi",
+        location: "Italy",
+        googleMapsUrl: "https://goo.gl/maps/5ve1En3zPPoUrjvz8",
+        startDate: "12 Sep, 2022",
+        endDate: "28 Sep, 2022",
+        description: `Amalfi is a town in a dramatic natural setting below steep cliffs on Italy's southwest coast. The Amalfi Coast is famous for its picturesque seaside towns, cliffs, and beaches. It's known for its stunning coastline and colorful villages with steep and narrow streets.`,
+        imageUrl: "italy-seashore.jpg"
+    },
+    {
+        id: 2,
+        title: "Ulun Danu Beratan Temple",
+        location: "Indonesia",
+        googleMapsUrl: "https://goo.gl/maps/syUrJT5ofQy1zSgFA",
+        startDate: "1 Apr, 2022",
+        endDate: "19 Apr, 2022",
+        description: "Pura Ulun Danu Beratan, or Pura Bratan, is a major Hindu Shaivite temple in Bali, Indonesia. The temple complex is on the shores of Lake Bratan in the mountains near Bedugul.",
+        imageUrl: "ulun-danu-beratan.jpg"
+    },
+    {
+        id: 3,
+        title: "La Digue",
+        location: "Seychelless",
+        googleMapsUrl: "https://goo.gl/maps/DhW8NJjJdTx8NBzh8",
+        startDate: "19 Dec, 2021",
+        endDate: "8 Jan, 2021",
+        description: "La Digue is an island in the Seychelles, in the Indian Ocean off East Africa. It's known for its beaches, like Anse Source d'Argent, dotted with granite boulders, on the west coast.",
+        imageUrl: "la-digue.jpg"
+    }
+]
